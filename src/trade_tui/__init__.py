e62036efb9466cacc4347a2f8e6307ee1a8915b9
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-12-07 22:30:18 +0100

    Update again for package

--- src/trade_tui/__init__.py
+++ src/trade_tui/__init__.py
@@ -1,2 +1,9 @@
-def main() -> None:
-    print("Hello from trade-tui!")
+import os
+from .util.config import config
+from .menus.main_menu import main_menu
+
+
+async def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    config.load_from_file()
+    await main_menu()
